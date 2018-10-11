08bde985dc276838a9d558858bbad1f4095c74b0
denvil <denvil@ecxol.net>
2018-10-11 13:02:10 +0300

    Added keyring to requirements

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
     install_requires=[
         'Click',
         'openerp_proxy',
-        'colorama'
+        'colorama',
+        'keyring'
     ],
     entry_points='''
         [console_scripts]
